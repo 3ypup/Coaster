507f07ce89b28403589704e45518f6e1470bc0c1
3ypUP <Posilay.pochashe@gmail.com>
2017-10-26 10:59:40 +0300

    added image and photo...contacts

--- app1.rb
+++ app1.rb
@@ -1,59 +1,23 @@
 require "sinatra"
+require 'sinatra/reloader'
 
 get "/" do
 	erb :index
 end
 
-
-get "/contacts" do
-	
-
-	@title = "contacts"
-	@message = "89265699563"
-
-	erb :message
+get '/photo' do
+  erb :photo
 end
 
-get "/FAQ" do
-	@title = "FAQ"
-	@message = "Under Construction"
 
-	erb :message
+get '/video' do
+  "Hello World"
 end
 
-post "/" do 
-	@user_name = params[:user_name]
-	@phone = params[:phone]
-	@date_time = params[:date_time]
-	
-
-	if @user_name && @phone == "admin"
-				
-
-				@f= File.open("users.txt", "r")
-				@title = "Client list"
-				@message = "Users:"
-				@list = {}
-				n = 0
-				@f.each_line do |line|
-				n=n+1
-				@list[n] = line
-				end
-
-				@f.close
-				erb :message
-				
-	else
-				@title = "Thank you"
-				@message = "Dear #{@user_name}, we will be waiting for you at #{@date_time}, your phone number is #{@phone}"
-				@list = {}
-				f = File.open "users.txt", "a"
-				f.write "User: #{@user_name}, Phone: #{@phone}, Date: #{@date_time}\n"
-				f.close
-
-
-
-				erb :message
-	end
-	
+get '/blog' do
+  "Hello World"
 end
+
+
+
+	
